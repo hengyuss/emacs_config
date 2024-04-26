7eb9a03de25e227c5afe042932808904b9af1707
root <root@DESKTOP-OPTI8OD>
2024-04-26 13:56:10 +0800

    add plugin yasnippet

--- init.el
+++ init.el
@@ -214,4 +214,30 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
   (setq company-selection-wrap-around t)
   (setq company-transformers '(company-sort-by-occurrence))) ; 根据选择的频率进行排序，读者如果不喜欢可以去掉
 
+(use-package yasnippet
+  :ensure t
+  :hook
+  (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all)
+  ;; add company-yasnippet to company-backends
+  (defun company-mode/backend-with-yas (backend)
+    (if (and (listp backend) (member 'company-yasnippet backend))
+	backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
+  (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+  ;; unbind <TAB> completion
+  (define-key yas-minor-mode-map [(tab)]        nil)
+  (define-key yas-minor-mode-map (kbd "TAB")    nil)
+  (define-key yas-minor-mode-map (kbd "<tab>")  nil)
+  :bind
+  (:map yas-minor-mode-map ("S-<tab>" . yas-expand)))
+
+(use-package yasnippet-snippets
+  :ensure t
+  :after yasnippet)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+
  (provide 'init)
